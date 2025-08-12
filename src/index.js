59012c3cee091d5a52fa580be643bcc63854624a
MichaelHokl <mhokl1997@gmail.com>
2025-08-12 18:40:58 +0200

    Added an onClick funtion to .game and also finalized the styling

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const gamesData = [
     title: "Carcassonne",
     players: "2-5",
     price: 35,
-    photo: "public/images/carcassonne.png",
+    photo: "images/Carcassonne.png",
     playtime: 45,
     soldOut: false,
   },
@@ -90,9 +90,9 @@ function Header() {
   return (
     <header>
       <h1>welcome to tabletop treasures</h1>
-      <p className="subheading">
+      <h2 className="subheading">
         We have the best games for the best gaming memories
-      </p>
+      </h2>
     </header>
   );
 }
@@ -101,32 +101,43 @@ function GameList() {
   const games = gamesData;
   const gamesLength = games.length;
 
-  return (
-    <ul className="game-list">
+  return gamesLength > 0 ? (
+    <ul className="gamelist">
       {games.map((game) => (
         <Game gameObj={game} key={game.title} />
       ))}
     </ul>
+  ) : (
+    <p>Sorry, we currently don't have any games.</p>
   );
 }
 
 function Game({ gameObj }) {
   const players = gameObj.players;
   const playTime = gameObj.playtime;
+  const price = gameObj.price;
+
+  function alerting() {
+    gameObj.soldOut
+      ? alert("This Game Is Sold Out!")
+      : alert("Now you would be redirected to the product page!");
+  }
 
   return (
-    <li className="game">
+    <li
+      className={gameObj.soldOut ? "game sold-out" : "game"}
+      onClick={() => alerting()}
+    >
       <img src={gameObj.photo} alt={gameObj.title} />
-      <div>
+      <div className="game-detail-container">
         <h3>{gameObj.title}</h3>
-        <DetailsList players={players} playTime={playTime} />
-        <p className="price">${gameObj.price}</p>
+        <DetailsList players={players} playTime={playTime} price={price} />
       </div>
     </li>
   );
 }
 
-function DetailsList({ players, playTime }) {
+function DetailsList({ players, playTime, price }) {
   return (
     <ul className="details">
       <li>
@@ -136,7 +147,12 @@ function DetailsList({ players, playTime }) {
       </li>
       <li>
         <p>
-          Duration: <span>{playTime}</span>
+          Duration: <span>{playTime} minutes</span>
+        </p>
+      </li>
+      <li>
+        <p>
+          Price: <span>${price}</span>
         </p>
       </li>
     </ul>
@@ -149,12 +165,11 @@ function Footer() {
   const closeHour = 18;
   const time = new Date().getHours();
   const isOpen = time >= openHour && time <= closeHour;
-  console.log(time);
   return (
     <footer>
       <p>
         {isOpen
-          ? `We are currently open! Come visit us until ${closeHour}. For questions please call ${phoneNumber}`
+          ? `We are currently open! Come visit us until ${closeHour}:00. For questions please call ${phoneNumber}`
           : `We are currently closed! Please come visit us tomorrow from ${openHour}:00 until ${closeHour}:00. Thank You`}
       </p>
     </footer>
